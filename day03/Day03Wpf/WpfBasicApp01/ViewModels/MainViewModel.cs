d64c95202b2df6f3b0b8b0d379c17affc67ff356
Naiery0 <molly724@naver.com>
2025-05-12 17:17:23 +0900

    3일차 커밋

---
+++ day03/Day03Wpf/WpfBasicApp01/ViewModels/MainViewModel.cs
@@ -0,0 +1,62 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace WpfBasicApp01.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        // NLog 객체 생성
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        #region 속성영역
+        private string _greeting;
+        public string Greeting
+        {
+            get => _greeting;
+            set => SetProperty(ref _greeting, value);
+        }
+
+        private string _currentTime;
+        public string CurrentTime
+        {
+            get => _currentTime;
+            set => SetProperty(ref _currentTime, value);
+        }
+
+        private readonly DispatcherTimer _timer; 
+
+        #endregion
+
+        public MainViewModel()
+        {
+            _logger.Info("뷰모델 시작");
+            Greeting = "Hello WPF MVVM!";
+
+            CurrentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"); // 초반렉
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += (sender, e) =>
+            {
+                CurrentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                Debug.WriteLine($"[DEBUG] {CurrentTime}");
+                _logger.Info($"[DEBUG] {CurrentTime}");
+            }; // 람다식
+            _timer.Start();
+
+        }
+
+        //private void _timer_Tick(object? sender, EventArgs e)
+        //{
+        //    CurrentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        //    Debug.WriteLine($"[DEBUG] {CurrentTime}");
+        //}
+    }
+}
---
+++ day03/Day03Wpf/WpfBookRentalShop01/ModelViews/BookGenreViewModel.cs
@@ -0,0 +1,110 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MySql.Data.MySqlClient;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using WpfBookRentalShop01.Models;
+
+namespace WpfBookRentalShop01.ModelViews
+{
+    public partial class BookGenreViewModel : ObservableObject
+    {
+        private ObservableCollection<Genre> _genres;
+
+        private Genre _selectedGenre;
+        public Genre SelectedGenre
+        {
+            get => _selectedGenre;
+            set {
+                SetProperty(ref _selectedGenre, value);
+                _isUpdate = true; // 수정할 상태다
+            }
+        }
+
+        private bool _isUpdate;
+
+        public BookGenreViewModel()
+        {
+            _isUpdate = false;
+            LoadGridFromDb();
+        }
+
+
+        public ObservableCollection<Genre> Genres
+        {
+            get => _genres;
+            set => SetProperty(ref _genres, value);
+        }
+
+        private void LoadGridFromDb()
+        {
+
+            string connecetionString = "Server=localhost;Database=bookrentalshop;Uid=root;Pwd=12345;Charset=utf8;";
+            string query = "SELECT division, names FROM divtbl";
+
+            ObservableCollection<Genre> genres = new ObservableCollection<Genre>();
+
+            using (MySqlConnection conn = new MySqlConnection(connecetionString)) { 
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var division = reader.GetString("division");
+                    var names = reader.GetString("names");
+
+                    genres.Add(new Genre
+                    {
+                        Division = division,
+                        Names = names
+                    });
+                }
+            }
+
+            Genres = genres;
+
+        }
+        [RelayCommand]
+        public void SetInit()
+        {
+            _isUpdate = false ;
+            SelectedGenre = null;
+        }
+
+        [RelayCommand]
+        public void SaveData()
+        {
+
+        }
+
+        [RelayCommand]
+        public void DelDate()
+        {
+            if (!_isUpdate)
+            {
+                MessageBox.Show("선택된 데이터가 아니면 삭제할 수 없습니다.");
+                return;
+            }
+            string connecetionString = "Server=localhost;Database=bookrentalshop;Uid=root;Pwd=12345;Charset=utf8;";
+            string query = "DELETE FROM divtbl WHERE division = @division";
+
+            ObservableCollection<Genre> genres = new ObservableCollection<Genre>();
+
+            using (MySqlConnection conn = new MySqlConnection(connecetionString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@division", SelectedGenre.Division);
+
+                int resultCnt = cmd.ExecuteNonQuery(); // 삭제한 건수만큼 리턴
+
+                if (resultCnt > 0) { MessageBox.Show("삭제성공"); }
+                else { MessageBox.Show("삭제실패"); }
+            }
+        }
+        
+    }
+}
